fd0f187504e77bc7f11ed253ecdfeddeadffcd4c
TheMaoci <themaoci@gmail.com>
2023-02-14 00:48:51 +0100

    Create waypoint_portal.lua

---
+++ data/scripts/actions/waypoint_portal.lua
@@ -0,0 +1,60 @@
+local waypoint_portal = Action()
+function waypoint_portal.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	print(GameConfig.Waypoint.Config.PlayerStorageValue + item:getUniqueId());
+  local isDev = player:getAccountType() == ACCOUNT_TYPE_GOD
+  -- add this waypoint to player saved waypoints 
+  if GameConfig.Waypoint.Config.EnableSavingWaypoints then
+    if player:getstoragevalue(storagevalues + item:getuniqueid()) ~= 1 then
+      player:setstoragevalue(storagevalues + item:getuniqueid(), 1)
+    end
+  end
+  
+  player:registerEvent("WaypointPortal_Modal_Window")
+
+  local title = "Waypoint Globe - Teleporter"
+  local message = "Choose visided waypoint:"
+    
+  local window = ModalWindow(item.actionid, title, message)
+  window:addButton(100, "Teleport")
+  window:addButton(101, "Close")
+  
+  for id, data in pairs(GameConfig.Waypoint.Locations) do 
+    if item:getUniqueId() == id then
+      goto continue
+    end
+    if isDev or player:getStorageValue(GameConfig.Waypoint.Config.PlayerStorageValue + item:getUniqueId()) == 1 then
+      local devText = ""
+      if isDev and player:getStorageValue(GameConfig.Waypoint.Config.PlayerStorageValue + item:getUniqueId()) == 1 then
+        devText = " (Known)"
+      end
+      window:addChoice(id - 1000, data.Name .. devText)
+    end
+    ::continue::
+  end
+
+  window:setDefaultEnterButton(100)
+  window:setDefaultEscapeButton(101)
+  window:sendToPlayer(player)
+  
+	return true
+end
+waypoint_portal:aid(GameConfig.Waypoint.Config.ObjectActionId) -- item action id you want to use for this menu!!!
+waypoint_portal:register()
+
+
+local modalWaypointPortalObject = CreatureEvent("WaypointPortal_Modal_Window")
+modalWaypointPortalObject:type("modalwindow")
+
+function modalWaypointPortalObject.onModalWindow(player, modalWindowId, buttonId, choiceId)
+    player:unregisterEvent("WaypointPortal_Modal_Window")
+      if buttonId == 100 then
+        if GameConfig.Waypoint.Locations[1000 + choiceId] ~= nil then
+          player:getPosition():sendMagicEffect(GameConfig.Waypoint.Config.TeleportEffectOut)
+          player:teleportTo(GameConfig.Waypoint.Locations[1000 + choiceId].pos)
+          player:getPosition():sendMagicEffect(GameConfig.Waypoint.Config.TeleportEffectIn)
+        end
+      end
+    return true
+end
+
+modalWaypointPortalObject:register()
