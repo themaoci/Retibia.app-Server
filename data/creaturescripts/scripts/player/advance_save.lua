5990e3f23ec190751ae321e9dedfe6a17d55791e
TheMaoci <themaoci@gmail.com>
2023-02-18 02:57:12 +0100

    display skill, level up and save profile of player on level up of each skill and level.
    made incase of game server crashing

--- data/creaturescripts/scripts/player/advance_save.lua
+++ data/creaturescripts/scripts/player/advance_save.lua
@@ -1,25 +1,45 @@
 local config = {
-	heal = false,
-	save = true,
-	effect = true
+    savePlayer = true,
+    OnLevelPlayerSetHpManaFull = false,
+    OnMagLevelPlayerSetManaFull = false,
+    skillsTable = {
+        [SKILL__LEVEL] = { enabled = true, text = "LEVEL UP", textColor = TEXTCOLOR_WHITE},
+        [SKILL__MAGLEVEL] = { enabled = true, text = "MAGIC UP", textColor = TEXTCOLOR_BLUE},
+        [SKILL_AXE] = { enabled = true, text = "AXE UP", textColor = TEXTCOLOR_ORANGE},
+        [SKILL_CLUB] = { enabled = true, text = "CLUB UP", textColor = TEXTCOLOR_ORANGE},
+        [SKILL_SWORD] = { enabled = true, text = "SWORD UP", textColor = TEXTCOLOR_ORANGE},
+        [SKILL_SHIELD] = { enabled = true, text = "SHIELDING UP", textColor = TEXTCOLOR_ORANGE},
+        [SKILL_DISTANCE] = { enabled = true, text = "DISTANCE UP", textColor = TEXTCOLOR_ORANGE}   
+    }
 }
 
 function onAdvance(player, skill, oldLevel, newLevel)
-	if skill ~= SKILL_LEVEL or newLevel <= oldLevel then
-		return true
-	end
+	if(skill == SKILL__EXPERIENCE) then
+        return true
+    end
 
-	if config.effect then
-		player:getPosition():sendMagicEffect(math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE))
-		player:say('LEVEL UP!', TALKTYPE_MONSTER_SAY)
-	end
+    local skillTable = config.skillsTable[skill]
+    if not skillTable then
+        return true
+    end
 
-	if config.heal then
-		player:addHealth(player:getMaxHealth())
-	end
+    if not skillTable.enabled then
+        return true
+    end
 
-	if config.save then
-		player:save()
-	end
-	return true
+    if skill == SKILL__LEVEL and config.OnLevelPlayerSetHpManaFull then
+        doCreatureAddHealth(cid, getCreatureMaxHealth(cid) - getCreatureHealth(cid))
+        doCreatureAddMana(cid, getCreatureMaxMana(cid) - getCreatureMana(cid))
+    end
+    if skill == SKILL__MAGLEVEL and config.OnMagLevelPlayerSetManaFull) then
+        doCreatureAddMana(cid, getCreatureMaxMana(cid) - getCreatureMana(cid))
+    end
+    
+    doSendAnimatedText(getPlayerPosition(cid), skillTable.text, skillTable.textColor)
+
+    if config.savePlayer then
+        doPlayerSave(cid, true)
+    end
+
+    return true
 end
