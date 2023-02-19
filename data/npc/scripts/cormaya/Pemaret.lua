ca75ffd5d89adb2f1dc715e025f47ad73d739f41
TheMaoci <themaoci@gmail.com>
2023-02-19 15:08:41 +0100

    adding cormaya npc's

---
+++ data/npc/scripts/cormaya/Pemaret.lua
@@ -0,0 +1,60 @@
+ local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)		 end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)		 end
+function onCreatureSay(cid, type, msg)	npcHandler:onCreatureSay(cid, type, msg) end
+function onThink()						npcHandler:onThink()					 end
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+	local player = Player(cid)
+	if msgcontains(msg, "marlin") then
+		if player:getItemCount(7963) > 0 then
+			npcHandler:say("WOW! You have a marlin!! I could make a nice decoration for your wall from it. May I have it?", cid)
+			npcHandler.topic[cid] = 1
+		end
+	elseif msgcontains(msg, "yes") and npcHandler.topic[cid] == 1 then
+		if player:removeItem(7963, 1) then
+			npcHandler:say("Yeah! Now let's see... <fumble fumble> There you go, I hope you like it!", cid)
+			player:addItem(7964, 1)
+		else
+			npcHandler:say("You don't have the fish.", cid)
+		end
+		npcHandler.topic[cid] = 0
+	end
+	if msgcontains(msg, "no") and npcHandler.topic[cid] == 1 then
+		npcHandler:say("Then no.", cid)
+		npcHandler.topic[cid] = 0
+	end
+	return true
+end
+
+-- Travel
+local function addTravelKeyword(keyword, text, cost, destination)
+	local travelKeyword = keywordHandler:addKeyword({keyword}, StdModule.say, {npcHandler = npcHandler, text = 'Do you seek a passage to ' .. keyword:titleCase() .. ' for |TRAVELCOST|?', cost = cost, discount = 'postman'})
+		travelKeyword:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = true, cost = cost, discount = 'postman', destination = destination})
+		travelKeyword:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, text = 'Maybe later.', reset = true})
+end
+
+-- Kick
+keywordHandler:addKeyword({'kick'}, StdModule.kick, {npcHandler = npcHandler, text = 'What do you mean by that. I\'m not gonna kick you that would be rude.'})
+
+-- Basic
+keywordHandler:addKeyword({'edron'}, StdModule.say, {npcHandler = npcHandler, text = 'Oh well... that will be hard to do... if even possible. everything is twisted now.'})
+keywordHandler:addKeyword({'eremo'}, StdModule.say, {npcHandler = npcHandler, text = 'Oh yes that small island i think you can got there by using a small boat underground.'})
+keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, text = 'I\'m a fisherman and I take along people to Edron. You can also buy some fresh fish.'})
+keywordHandler:addKeyword({'captain'}, StdModule.say, {npcHandler = npcHandler, text = 'I\'m a fisherman and I take along people to Edron. You can also buy some fresh fish.'})
+keywordHandler:addKeyword({'fish'}, StdModule.say, {npcHandler = npcHandler, text = 'My fish is of the finest quality you could find. Ask me for a trade to check for yourself.'})
+keywordHandler:addKeyword({'cormaya'}, StdModule.say, {npcHandler = npcHandler, text = 'It\'s a lovely and peaceful isle. Did you already visit the nice sandy beach?'})
+keywordHandler:addKeyword({'name'}, StdModule.say, {npcHandler = npcHandler, text = 'My name is Pemaret, the fisherman.'})
+
+npcHandler:setMessage(MESSAGE_GREET, "Greetings, young man. Looking for something, |PLAYERNAME|?")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye.")
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
