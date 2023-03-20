0393cbb0888dd679b056aaea0e89a8796aa4d6a9
TheMaoci <themaoci@gmail.com>
2023-03-20 15:34:33 +0100

    keeping track on storage values used in map

---
+++ data/storageValues.lua
@@ -0,0 +1,44 @@
+-- all storage values will be saved here.
+
+-- 60800 - waypoint start storage value
+-- waypoints increase value by 1000 + number of waypoints on the Mages Outpost
+-- reserved `100`
+
+-- rewardChest - give contents of the chest.
+-- starting from 7100 
+-- reserved `899`
+-- rook chest quests - storage range
+-- start from 7000 ends on 7099
+
+-- anti damage on login - 50722
+
+-- disgusting killer - unjustified killing
+-- 62000
+
+-- cupcake storage
+-- 19043
+-- largeseashell storage
+-- 19044
+-- wall mirror storage
+-- 19045
+
+
+-- Rashid missions storages
+-- 2001 - 2006
+
+-- propably not gonna be used but better to keep track on them
+-- 30002 -- druid rod
+
+
+-- stepin achivements
+
+-- 10101 - entered mage outpost
+-- 10102 - forst time in fury maze
+-- 10103 - first time in desert island
+-- 10104 - found secret underground place in desert island minotaur "tower"
+-- 10105 - visited demon area
+-- 10106 - hidden area discovered
+-- 10107 - discovered Yielothax nest
+-- 10108 - discovered underground crypt
+-- 10109 - discovered Aria
+-- 10110 - discovered unknown crypt or cathedral ?
