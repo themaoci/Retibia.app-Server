1033b4ae1bd87a540182206c500f1d8c15294b27
TheMaoci <themaoci@gmail.com>
2023-03-20 15:30:57 +0100

    debug staff

---
+++ data/lib/debug/functions.lua
@@ -0,0 +1,29 @@
+DEBUG = {}
+
+function DEBUG.print_r ( t ) 
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"  ")
+end
+function DEBUG.SpellLogger(Config)
+    --print("['" .. Config.NAME .. "'] =  { ['levelDivider'] = 5, ['min'] = { ['skillMul'] = 0.01, ['value'] = 10 }, ['max'] = { ['skillMul'] = 0.01, ['value'] = 10 }")
+end
